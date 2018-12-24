aa1f5cd1565388ee7c0b3b7f0cf9658548e4a492
himanshu gurjar <himanshuforforms@gmail.com>
2018-12-24 19:11:09 +0530

    django_first_demo_website

---
+++ personal/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+        url('^$', views.index, name = 'index'),
+        url('^contact/$', views.contact, name = 'Contact' ),
+        ]
